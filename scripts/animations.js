85d62cf8a844acfcd81122e271fb5f2ab6f5d120
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-09-11 23:48:05 -0300

    Add text to shapes, separate code in different files

---
+++ scripts/animations.js
@@ -0,0 +1,39 @@
+/**
+ * @author Tomas Perez Molina
+ */
+
+const popup = function(){
+    const ratio = 's1.2';
+    this.toFront();
+    this.attr({
+        cursor: 'pointer',
+        fill: '#990000',
+        stroke: '#000000',
+        'stroke-width': '2'
+    });
+    this.animate({
+        transform: ratio
+    }, 100);
+    this.text.toFront();
+    this.text.animate({
+        transform: ratio
+    }, 100);
+};
+
+const popDown = function(){
+    this.attr({
+        fill: '#15E0FF'
+    });
+    this.animate({
+        transform: 's1'
+    }, 100);
+    this.text.animate({
+        transform: 's1'
+    }, 100);
+};
+
+const onClick = function(){
+    this.animate({
+        fill: 'green',
+    }, 100);
+};
---
+++ scripts/shapesUtilities.js
@@ -0,0 +1,61 @@
+/**
+ * @author Tomas Perez Molina
+ */
+
+const shapesFromSvg = function(filePath) {
+    return new Promise((resolve, reject) => {
+        $.get(filePath).done((data) => {
+            const paths = [];
+
+            $(data).find('polygon, rect, path').each(function (i, shape) {
+                let queryShape = $(shape);
+                let d = '';
+                if (queryShape.is('path')) {
+                    d = queryShape.attr('d').trim();
+                }
+                else if (queryShape.is('polygon')) {
+                    d = 'M' + queryShape.attr('points').trim() + 'z';
+                }
+                else if(queryShape.is('rect')) {
+                    const x = queryShape.attr('x');
+                    const y = queryShape.attr('y');
+                    const w = queryShape.attr('width');
+                    const h = queryShape.attr('height');
+
+                    d = 'M' + x + ',' + y + 'h' + w + 'v' + h + 'h' + -w + 'z';
+                }
+                paths.push({d: d, id: queryShape.attr('id')});
+            });
+
+            resolve(paths);
+        }).fail(() => reject(new Error('Could not find file')));
+    });
+};
+
+async function drawShapes(filePath, color) {
+    const shapes = await shapesFromSvg(filePath);
+
+    return shapes.map(shape => {
+        return map.path(shape.d).attr({
+            'stroke-width': '2',
+            'stroke-opacity': '1',
+            'fill': color
+        }).data({'id': shape.id});
+    });
+}
+
+const setText = function(shape){
+    const box = shape.getBBox();
+    const text = map.text(box.x + box.width/2, box.y + box.height/2, shape.data().id);
+    text.node.setAttribute("pointer-events", "none");
+    shape.text = text;
+};
+
+const createSets = function(paper, setIds){
+    return setIds.map(id => {
+        const set = paper.set();
+        set.data = {'id': id};
+        return set;
+    })
+};
+
