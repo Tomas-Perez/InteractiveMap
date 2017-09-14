9b3c964086779e281d82373935a270dcaf05203e
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-09-13 23:11:59 -0300

    JS Standard and ESLint for everyone

--- scripts/map.js
+++ scripts/map.js
@@ -1,26 +1,26 @@
 /**
  * @author Tomas Perez Molina
  */
-const raph = require('raphael');
-const popper = require('popper.js');
-const utilities = require('./shapesUtilities.js');
-const animations = require('./animations.js');
+const raph = require('raphael')
+const popper = require('popper.js')
+const utilities = require('./shapesUtilities.js')
+const animations = require('./animations.js')
 
-const map = raph('map', '960', '560');
-const sets = utilities.createSets(map, ['A', 'B', 'C', 'D']);
+const map = raph('map', '960', '560')
+const sets = utilities.createSets(map, ['A', 'B', 'C', 'D'])
 
-const shapes = utilities.drawShapes('./svg/map.svg', map, '#15E0FF');
+const shapes = utilities.drawShapes('./svg/map.svg', map, '#15E0FF')
 
 shapes.then(raphaelShapes => raphaelShapes.forEach(shape => {
-    utilities.setText(map, shape);
-    const set = sets.find(set => set.data.id === shape.data('id')[0]);
-    set.push(shape);
-}));
+  utilities.setText(map, shape)
+  const set = sets.find(set => set.data.id === shape.data('id')[0])
+  set.push(shape)
+}))
 
 shapes.then(() => {
-    sets.filter(set => set.data.id !== 'D').forEach(set => {
-        set.mouseover(animations.popup);
-        set.mouseout(animations.popDown);
-        set.click(animations.onClick);
-    })
-});
+  sets.filter(set => set.data.id !== 'D').forEach(set => {
+    set.mouseover(animations.popup)
+    set.mouseout(animations.popDown)
+    set.click(animations.onClick)
+  })
+})
--- scripts/shapesUtilities.js
+++ scripts/shapesUtilities.js
@@ -2,67 +2,65 @@
  * @author Tomas Perez Molina
  */
 
-const $ = require('jquery');
+const $ = require('jquery')
 
-const shapesFromSvg = function(filePath) {
-    return new Promise((resolve, reject) => {
-        $.get(filePath).done((data) => {
-            const paths = [];
+const shapesFromSvg = function shapesFromSvg (filePath) {
+  return new Promise((resolve, reject) => {
+    $.get(filePath).done((data) => {
+      const paths = []
 
-            $(data).find('polygon, rect, path').each(function (i, shape) {
-                let queryShape = $(shape);
-                let d = '';
-                if (queryShape.is('path')) {
-                    d = queryShape.attr('d').trim();
-                }
-                else if (queryShape.is('polygon')) {
-                    d = 'M' + queryShape.attr('points').trim() + 'z';
-                }
-                else if(queryShape.is('rect')) {
-                    const x = queryShape.attr('x');
-                    const y = queryShape.attr('y');
-                    const w = queryShape.attr('width');
-                    const h = queryShape.attr('height');
+      $(data).find('polygon, rect, path').each((i, shape) => {
+        const queryShape = $(shape)
+        let d = ''
+        if (queryShape.is('path')) {
+          d = queryShape.attr('d').trim()
+        } else if (queryShape.is('polygon')) {
+          d = `M${queryShape.attr('points').trim()}z`
+        } else if (queryShape.is('rect')) {
+          const x = queryShape.attr('x')
+          const y = queryShape.attr('y')
+          const w = queryShape.attr('width')
+          const h = queryShape.attr('height')
 
-                    d = 'M' + x + ',' + y + 'h' + w + 'v' + h + 'h' + -w + 'z';
-                }
-                paths.push({d: d, id: queryShape.attr('id')});
-            });
+          d = `M${x},${y}h${w}v${h}h${-w}z`
+        }
+        paths.push({ d, id: queryShape.attr('id') })
+      })
 
-            resolve(paths);
-        }).fail(() => reject(new Error('Could not find file')));
-    });
-};
-
-async function drawShapes(filePath, map, color) {
-    const shapes = await shapesFromSvg(filePath);
-
-    return shapes.map(shape => {
-        return map.path(shape.d).attr({
-            'stroke-width': '2',
-            'stroke-opacity': '1',
-            'fill': color
-        }).data({'id': shape.id});
-    });
+      resolve(paths)
+    }).fail(() => reject(new Error('Could not find file')))
+  })
 }
 
-const setText = function(map, shape){
-    const box = shape.getBBox();
-    const text = map.text(box.x + box.width/2, box.y + box.height/2, shape.data().id);
-    text.node.setAttribute("pointer-events", "none");
-    shape.text = text;
-};
+const drawShapes = async function drawShapes (filePath, map, color) {
+  const shapes = await shapesFromSvg(filePath)
 
-const createSets = function(paper, setIds){
-    return setIds.map(id => {
-        const set = paper.set();
-        set.data = {'id': id};
-        return set;
-    })
-};
+  return shapes.map(shape => map.path(shape.d).attr(
+    {
+      'stroke-width': '2',
+      'stroke-opacity': '1',
+      fill: color
+    }).data({ id: shape.id })
+  )
+}
+
+const setText = function setText (map, shape) {
+  const box = shape.getBBox()
+  const text = map.text(box.x + box.width / 2, box.y + box.height / 2, shape.data().id)
+  text.node.setAttribute('pointer-events', 'none')
+  shape.text = text
+}
+
+const createSets = function createSets (paper, setIds) {
+  return setIds.map((id) => {
+    const set = paper.set()
+    set.data = { id }
+    return set
+  })
+}
 
 module.exports = {
-    drawShapes: drawShapes,
-    setText: setText,
-    createSets: createSets
+  drawShapes,
+  setText,
+  createSets
 }
